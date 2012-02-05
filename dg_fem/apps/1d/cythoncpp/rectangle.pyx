cc6ffdda47151889b864c97059a3ea27e45f88b9
James Rossmanith <imsejae@yahoo.com>
2012-02-05 19:53:40 +0300

    Modified a couple of files.

--- dg_fem/apps/1d/cythoncpp/rectangle.pyx
+++ dg_fem/apps/1d/cythoncpp/rectangle.pyx
@@ -35,15 +35,3 @@ cdef class Rectangle:
                                            self.thisptr.y0, 
                                            self.thisptr.x1, 
                                            self.thisptr.y1)
-
-"""
-cdef Rectangle r = Rectangle(1, 2, 3, 4)
-print r
-print "Original area:", r.getArea()
-r.move(1,2)
-print r
-print "Area is invariante under rigid motions:", r.getArea()
-r += Rectangle(0,0,1,1)
-print r
-print "Now the aread is:", r.getArea()
-"""
--- dg_fem/apps/1d/cythoncpp/userect.py
+++ dg_fem/apps/1d/cythoncpp/userect.py
@@ -5,7 +5,7 @@ print r
 print "Original area:", r.getArea()
 r.move(1,2)
 print r
-print "Area is invariante under rigid motions:", r.getArea()
+print "Area is invariant under rigid motions:", r.getArea()
 r += Rectangle(0,0,1,1)
 print r
 print "Now the aread is:", r.getArea()
