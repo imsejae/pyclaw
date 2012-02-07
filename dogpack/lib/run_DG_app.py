1183c2fd8c540f2ca68080b71722d749dcb47ba9
Scott Moe <smeest1@gmail.com>
2012-02-07 04:08:09 -0800

    added python

--- dogpack/lib/run_DG_app.py
+++
@@ -1,7 +0,0 @@
-def run_DG_cpp():
-    # write the parameters file
-    outputdir = 'outputdir'
-    failure = os.system('dog.exe %s' % filename)
-    if failure:
-        print 'Could not run'
-    
