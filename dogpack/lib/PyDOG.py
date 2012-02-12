228788c7cb88b89570826e4f6d9bc35d55a4270c
Scott Moe <smeest1@gmail.com>
2012-02-11 22:09:08 -0800

    checked in python plotting stuff

--- dogpack/lib/PyDOG.py
+++ dogpack/lib/PyDOG.py
@@ -1,8 +1,12 @@
 import os
-
-def runDOG(outputdir='output'):
+import pyclaw
+import numpy as np
+def runDOG(iplot=False,htmlplot=False,outputdir='output'):
     # call dogpack and output to specified (or not) directory
     os.system('dog.exe -o %s' % outputdir)
-
+    if iplot:
+      pyclaw.plot.interactive_plot('_output','asciiy')
+    if htmlplot:  
+      pyclaw.plot.html_plot('_output','asciiy')
 #def load_DG_app(outputdir='output'):
         
